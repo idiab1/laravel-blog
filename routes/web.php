7c388070653320a42e535dd7ba9ffd4721f43627
islamdiab-stack <islamdiab967@gmail.com>
2021-03-19 12:10:06 +0200

    Create routes for about and contact pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', 'PagesController@getabout')->name('about');
+Route::get('/contact', 'PagesController@getcontact')->name('contact');
+
+
+
+
