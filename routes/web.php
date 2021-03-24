237936ab7e15709c0de62e8d488c42763a8e46c5
islamdiab-stack <islamdiab967@gmail.com>
2021-03-24 08:06:12 +0200

    Create authantication for login and registeration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,7 @@ Route::get('/contact', 'PagesController@getContact')->name('contact');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
