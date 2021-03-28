3e65e0d56c0a75c2c70bbb528555da70a6304c2e
islamdiab-stack <islamdiab967@gmail.com>
2021-03-29 00:30:55 +0200

    Edit on post card component

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,76 +9,84 @@
     <div class="col-md-9">
         <div class="row my-4">
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="row my-4">
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card post-card">
 
 
-                    <div class="card-body">
+                    <div class="card-body post-body">
                         <h5 class="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Read More</a>
+                        <a href="#" class="">Read More</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-md-3 my-4 bg-dark">
+    <div class="col-md-3 my-4 sidebar">
         <h4 class="posts-sidebar">Heading</h4>
+        <div class="search-form">
+            <form action="" method="">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Find your post">
+                    <button type="submit" class="">Find</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
