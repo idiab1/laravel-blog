85f819b928c410637158078c5fb8fa50c675a30d
islamdiab-stack <islamdiab967@gmail.com>
2021-03-28 17:24:00 +0200

    Edit on header.blade.php for add to it message

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,7 @@
-<header class="main-header">
+<header class="main-header text-center">
     <div class="container">
-        <div class="header-content"></div>
+        <div class="header-content">
+            <h1 class="h1 main-heading">Welcome to eblog</h1>
+        </div>
     </div>
 </header>
