078741b92617c2100ece430b5021e1338b12307e
islamdiab-stack <islamdiab967@gmail.com>
2021-04-02 06:42:48 +0200

    Edit on register structure

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,90 +39,93 @@
     </div> --}}
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="row">
-                <div class="col-md-6 bg-white">
-                    <div class="sign-form">
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
-                            <!-- Name -->
-                            <div class="form-group">
-                                <label for="name" class="col-md-10 col-form-label ">{{ __('Name') }}</label>
+            <div class="sign-form-container">
+                <div class="row">
+                    <div class="col-md-6 p-0">
+                        <div class="sign-form-box">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <!-- Name -->
+                                <div class="form-group">
+                                    <label for="name" class="col-md-12 col-form-label ">{{ __('Name') }}</label>
 
-                                <div class="col-md-8">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <div class="col-md-12">
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
-                                    @error('name')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
 
-                            <!-- Email -->
-                            <div class="form-group">
-                                <label for="email" class="col-md-10 col-form-label ">{{ __('E-Mail Address') }}</label>
+                                <!-- Email -->
+                                <div class="form-group">
+                                    <label for="email" class="col-md-12 col-form-label ">{{ __('E-Mail Address') }}</label>
 
-                                <div class="col-md-8">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <div class="col-md-12">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
 
-                            <!-- Password -->
-                            <div class="form-group">
-                                <label for="password" class="col-md-10 col-form-label ">{{ __('Password') }}</label>
+                                <!-- Password -->
+                                <div class="form-group">
+                                    <label for="password" class="col-md-12 col-form-label ">{{ __('Password') }}</label>
 
-                                <div class="col-md-8">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                    <div class="col-md-12">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
-                            </div>
 
-                            <!-- Confirm Password-->
-                            <div class="form-group">
-                                <label for="password-confirm" class="col-md-10 col-form-label ">{{ __('Confirm Password') }}</label>
+                                <!-- Confirm Password-->
+                                <div class="form-group">
+                                    <label for="password-confirm" class="col-md-12 col-form-label ">{{ __('Confirm Password') }}</label>
 
-                                <div class="col-md-8">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    <div class="col-md-12">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group mb-0">
-                                <div class="col-md-8">
-                                    <button type="submit" class="btn btn-primary sign-btn">
-                                        {{ __('Sign up') }}
-                                    </button>
+                                <div class="form-group mb-0">
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn btn-primary sign-btn">
+                                            {{ __('Sign up') }}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6 text-center bg-dark">
-                    <div class="sign-social-media">
-                        <ul class="list-unstyled">
-                            <li>
-                                <a href="#" class="btn btn-primary">Facebook</a>
-                            </li>
-                            <li>
-                                <a href="#" class="btn btn-primary">Google</a>
-                            </li>
-                        </ul>
+                    <div class="col-md-6 text-center p-0 sing-social-container">
+                        <div class="sign-social-media">
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#" class="btn btn-primary">Facebook</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="btn btn-primary">Google</a>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
 
                 </div>
-
             </div>
+
         </div>
 
     </div>
