4a93fab66e324b5b7a152bc5f42ec6f6bc7fea7d
islamdiab-stack <islamdiab967@gmail.com>
2021-04-02 19:32:25 +0200

    Create structure main section specially posts and add Comment on slidebar-posts

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,88 +5,185 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-md-9">
-        <div class="row my-4">
-            <div class="col-md-4">
-                <div class="card post-card">
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card post-card">
-
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card post-card">
-
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
+<div class="main-section">
+    <div class="row">
+        <div class="col-12">
+        <div class="main-section-info">
+            <h2 class="heading">Post Popular</h2>
         </div>
-        <div class="row my-4">
-            <div class="col-md-4">
-                <div class="card post-card">
 
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card post-card">
-
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card post-card">
-
-
-                    <div class="card-body post-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="">Read More</a>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
-    <div class="col-md-3 my-4 sidebar">
-        <h4 class="posts-sidebar">Heading</h4>
-        <div class="search-form">
-            <form action="" method="">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Find your post">
-                    <button type="submit" class="btn">Find</button>
+    <div class="row">
+        {{-- <div class="col-md-9">
+            <div class="row">
+                <div class="col-md">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </form>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+        <div class="col-md">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        {{-- <div class="col-md-3 sidebar-posts">
+            <h4 class="posts-sidebar">Heading</h4>
+            <div class="search-form">
+                <form action="" method="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Find your post">
+                        <button type="submit" class="btn">Find</button>
+                    </div>
+                </form>
+            </div>
+        </div> --}}
     </div>
 </div>
 @endsection
