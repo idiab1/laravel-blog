b052ccbb32709ff9e01952cf89a269ab9c986d29
islamdiab-stack <islamdiab967@gmail.com>
2021-04-03 03:01:22 +0200

    just update files

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <header class="main-header text-center">
         <div class="container">
             <div class="header-content">
-                <h1 class="h1 main-heading">Welcome to eblog</h1>
+                <h1 class="h1 main-heading">Welcome to eBlog</h1>
             </div>
         </div>
     </header>
