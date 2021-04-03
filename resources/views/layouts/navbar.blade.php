3bd8cda3463a16406f4290458c855a21477d4603
islamdiab-stack <islamdiab967@gmail.com>
2021-04-03 02:55:46 +0200

    Edit on Structure navbar where i added new button for create new post

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -34,15 +34,24 @@
                         </li>
                     @endif
                 @else
+
+                    <li class="nav-item">
+                        <a class="btn btn-new-post" href="#">{{ __('Write new post') }}</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a>
-
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">
+                                {{ __('Profile') }}
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                {{ __('Setting') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
