43ed357e0c24a8a4a1c22e251e1e8c46759b6230
islamdiab-stack <islamdiab967@gmail.com>
2021-04-05 00:00:06 +0200

    Add to header layout sub-header class and yield content

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 @section('header')
-    <header class="main-header text-center">
+    <header class="main-header sub-heading text-center">
         <div class="container">
             <div class="header-content">
-                <h1 class="h1 main-heading">Welcome to eBlog</h1>
+                <h1 class="h1 main-heading">@yield('header_content', 'Welcome to eBlog')</h1>
             </div>
         </div>
     </header>
