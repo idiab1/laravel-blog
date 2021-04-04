6a63a7d8ec18265ef42becbbfe017db5e219e6b1
islamdiab-stack <islamdiab967@gmail.com>
2021-04-05 00:01:32 +0200

    Create the main structure for profile layout

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,26 +1,43 @@
 @extends('layouts.app')
-
+{{-- Title --}}
 @section('title')
     {{__("Profile")}}
 @endsection
 
+@section('styles')
+@endsection
+
+@section('header_content')
+    {{__('profile')}}
+@endsection
+
+
 @section('content')
+
 <div class="profile-content">
     <div class="row">
         <div class="col-md">
             <div class="profile-container">
                 <form>
+                    <!-- Name -->
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Name">
+                    </div>
+                    <!-- Email address -->
                     <div class="form-group">
                         <label for="email">Email address</label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
                     </div>
-                    <!-- Gender -->
+                    <!-- Password -->
                     <div class="form-group">
-                        <label for="gender">Gender</label>
-                        <select class="form-control" id="gender" name="gender">
-                            <option>Male</option>
-                            <option>Female</option>
-                        </select>
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="password">
+                    </div>
+                    <!-- Confirm Password -->
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm Password</label>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="confirm password">
                     </div>
                     <!-- linkedin url -->
                     <div class="form-group">
@@ -31,6 +48,14 @@
                     <div class="form-group">
                         <label for="facebook">Facebook</label>
                         <input type="text" class="form-control" id="facebook" name="facebook" placeholder="name@example.com/username">
+                    </div>
+                    <!-- Gender -->
+                    <div class="form-group">
+                        <label for="gender">Gender</label>
+                        <select class="form-control" id="gender" name="gender">
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
                     </div>
                     <!-- Bio of user -->
                     <div class="form-group">
