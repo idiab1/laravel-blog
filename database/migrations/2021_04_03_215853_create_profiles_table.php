64ccb6fc996d9496c4e9e37069f6bc1b45e84f80
islamdiab-stack <islamdiab967@gmail.com>
2021-04-04 02:29:48 +0200

    add to profile migration some column like address, gender, bio, facebook_url, linkedin_url, and finally.. i added user_id for relationship between user and profile

--- database/migrations/2021_04_03_215853_create_profiles_table.php
+++ database/migrations/2021_04_03_215853_create_profiles_table.php
@@ -15,7 +15,14 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->string('gender')->nullable();
+            $table->string('address')->nullable();
+            $table->text('bio')->nullable();
+            $table->text('facebook_url')->nullable();
+            $table->text('linkedin_url')->nullable();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
