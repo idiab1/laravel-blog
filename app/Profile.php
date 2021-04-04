27af42984da588224980823c7a5c8c5b106dbae5
islamdiab-stack <islamdiab967@gmail.com>
2021-04-04 07:44:11 +0200

    Create relationship between profile and user where each of user has one of profile

--- app/Profile.php
+++ app/Profile.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    protected $table = "profiles";
+
+    protected $fillable= [
+        'gender', 'address', 'bio', 'facebook_url', 'linkedin_url', 'user_id'
+    ];
+
+    public function user(){
+
+        return $this->belongsTo('App\User', 'user_id');
+
+    }
+
+
 }
