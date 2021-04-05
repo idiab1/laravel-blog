5e6bc31a32a33bec71e8a2f9e985a47e8de559e2
islamdiab-stack <islamdiab967@gmail.com>
2021-04-05 10:31:18 +0200

    Create migration posts and add 9 columns for index, create, store, edit, update, destroy, trashed, hdelete, and restore

--- database/migrations/2021_03_30_001629_create_posts_table.php
+++ database/migrations/2021_03_30_001629_create_posts_table.php
@@ -15,6 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->longText('content');
+            $table->string('photo');
+            $table->string('slug');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
