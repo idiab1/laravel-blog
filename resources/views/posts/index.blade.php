cbe0658bd0d9ed9592f3fd5f6fb4eaa3ae3dc9cf
islamdiab-stack <islamdiab967@gmail.com>
2021-04-07 17:34:13 +0200

    Create main page for cotains all posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,190 @@
+@extends('layouts.app')
+{{-- Title --}}
+@section('title')
+    {{__("All Posts")}}
+@endsection
+
+@section('styles')
+@endsection
+
+@section('header_content')
+    {{__('All Posts')}}
+@endsection
+
+{{-- Breadcrumb --}}
+@section('breadcrumb')
+    <div aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{route('home')}}">eBlog</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Posts</li>
+        </ol>
+    </div>
+@endsection
+
+@section('content')
+<div class="section-posts">
+    <div class="row">
+        @if ($posts->count() > 0)
+            <div class="col-md">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">User</th>
+                                <th scope="col">Photo</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <th scope="row">{{$post->id}}</th>
+                                    <td>{{$post->title}}</td>
+                                    <td>{{$post->created_at->diffForhumans()}}</td>
+                                    <td>{{$post->user->name}}</td>
+                                    <td>
+                                        <img class="img-tumbnail" src="{{URL::asset($post->photo)}}" alt="{{$post->photo}}" width="100" height="100"/>
+                                    </td>
+                                    <td>
+                                        <a class="text-success" href="{{route('post.show', ['post' => $post->slug])}}"> <i class="fas fa-eye"></i></a>
+                                        <a class="text-primary px-2" href="{{route('post.edit', ['post' => $post->id])}}"> <i class="fas fa-edit"></i></a>
+                                        <form action="{{route('post.destroy', ['post' => $post->id])}}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn text-danger" type="submit"><i class="fas fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @else
+            <div class="col-md">
+                <div class="alert alert-danger">
+                    There are no Posts yet
+                </div>
+            </div>
+        @endif
+
+        {{-- <div class="col-md">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card post-card">
+
+                        <div class="card-body post-body">
+                            <h5 class="card-title post-title">Card title</h5>
+                            <span class="post-date">02/4/2021</span>
+                            <p class="card-text post-description">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-link btn-read-post">Read More</a>
+                        </div>
+                        <div class="card-footer post-footer">
+                            <ul class="list-unstyled">
+                                <li>Created by: Name</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+
+        {{-- <div class="col-md-3 sidebar-posts">
+            <h4 class="posts-sidebar">Heading</h4>
+            <div class="search-form">
+                <form action="" method="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Find your post">
+                        <button type="submit" class="btn">Find</button>
+                    </div>
+                </form>
+            </div>
+        </div> --}}
+    </div>
+</div>
+@endsection
