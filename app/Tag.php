d2b80f0d889e374dfadc03b55fce31f43a684110
islamdiab-stack <islamdiab967@gmail.com>
2021-04-07 17:26:32 +0200

    Create new files for migration, controller and model for tags and create new migration for relationship between posts and tag where posts belongs to many tags, in  normalization roles you should change any relation many : many, to 1 to many, so you should create new table calculate post_id and tag_id for connect to post and tag tables

---
+++ app/Tag.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    protected $table = "tags";
+    protected $fillable = [
+        'tag'
+    ];
+
+    // relationship between tags and post
+}
