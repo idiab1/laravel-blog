ce56988b9e693728ea900e933ea7dabc425c0ccc
islamdiab-stack <islamdiab967@gmail.com>
2021-04-15 17:41:51 +0200

    Edit on method index for get 12 post from Post model and pass data to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        $posts = Post::inRandomOrder()->limit(12)->get();
+        return view('pages.home', compact('posts'));
     }
 }
